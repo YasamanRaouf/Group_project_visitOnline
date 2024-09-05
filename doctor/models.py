0fdd0b37bc7d0d1b8c3dd6ee5fbab3ad7afb728e
Mahan MI (PC) <majlesi.mahan@gmail.com>
2024-09-05 21:30:54 +0330

    fix: :ambulance: Added availability JsonField

--- doctor/models.py
+++ doctor/models.py
@@ -2,8 +2,21 @@ from django.db import models
 
 
 class Doctor(models.Model):
+
+    availability_dict = {
+        'saturday': [],
+        'sunday': [],
+        'monday': [],
+        'tuesday': [],
+        'wednesday': [],
+        'thursday': [],
+        'friday': [],
+        'interval': 20,
+    }
+
     doctor_id = models.AutoField(primary_key=True)
     user = models.ForeignKey('user.User', on_delete=models.CASCADE)
     speciality = models.ForeignKey('user.Speciality', on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     is_active = models.BooleanField(default=True)
+    availability = models.JSONField(default=availability_dict)
