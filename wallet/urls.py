1f4779985a208ec336d13bf5549f59616be56b47
siamak kamanabroo <siamak.kamanabroo@gmail.com>
2024-09-09 18:59:50 +0330

    update wallet_detail url pattern

--- wallet/urls.py
+++ wallet/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.wallet_list, name='wallet_list'),
-    path('wallet/<int:wallet_id>/', views.wallet_detail, name='wallet_detail'),
+    path('<int:wallet_id>/', views.wallet_detail, name='wallet_detail'),
     path('create/', views.create_wallet, name='create_wallet'),
 ]
