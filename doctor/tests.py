dc1c9ca42602c27d65b1cd23bbe8690ce6d72748
i-ftm <ftm.nateghi@gmail.com>
2024-09-15 17:13:44 +0330

    doctor list test and doctor search test

--- doctor/tests.py
+++ doctor/tests.py
@@ -1,3 +1,43 @@
 from django.test import TestCase
+from model_bakery import baker
+from visitonline.doctor.models import Doctor
+from django.contrib.auth.models import User
+from django.urls import reverse
+
+class DoctorTest(TestCase):
+    def setUp(self) -> None:
+        self.user = baker.make(User, username='testuser', password='testpassword')
+        self.doctor = baker.make(Doctor, uuser=self.user ,specialty='testspecialty')
+
+    def tearDown(self) -> None:
+        self.user.delete()
+        self.doctor.delete()
+
+    def test_doctor_list_view(self):
+        self.client.login(username='testuser', password='testpassword')
+        response = self.client.get(reverse('Doctor_list'))  # نام URL را با نام صحیح جایگزین کنید
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'doctor/templates/doctorList.html')
+        self.assertContains(response, self.user.full_name)
+
+    def test_doctor_search_view(self):
+        self.client.login(username='testuser', password='testpassword')
+        response = self.client.get(
+            reverse('Doctor_Search') + '?search_item=Cardiology')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'doctor/templates/doctorSearch.html')
+        self.assertContains(response, self.user.full_name)
+
+    def test_doctor_search_view_no_results(self):
+        self.client.login(username='testuser', password='testpassword')
+        response = self.client.get(reverse('Doctor_Search') + '?search_item=NonExistentSpecialty')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'doctor/templates/doctorSearch.html')
+        self.assertContains(response, 'No doctors found')
+
+
 
 # Create your tests here.
