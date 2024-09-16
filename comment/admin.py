e2fff86b3702042d6a62f7e9715cfa7c6e7d0621
AMD921 <amdavoudi91@gmail.com>
2024-09-16 20:10:03 +0330

    todo: write tests

--- comment/admin.py
+++ comment/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Comment
 
-# Register your models here.
+
+def approve_comments(modeladmin, request, queryset):
+    queryset.update(is_approved=True)
+    modeladmin.message_user(request, "Selected comments have been approved.")
+
+approve_comments.short_description = "Approve selected comments"
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('user', 'doctor', 'visit', 'rating', 'created_at', 'is_approved')
+    search_fields = ('user__username', 'doctor__name', 'text')
+    list_filter = ('rating', 'created_at', 'is_approved')
+    actions = [approve_comments]
--- comment/views.py
+++ comment/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .forms import CommentForm
+from .models import Doctor, Visit
 
 # Create your views here.
+from django.shortcuts import render, get_object_or_404
+from .models import Comment, Doctor
+
+def doctor_comments(request, doctor_id):
+    # Retrieve the doctor based on URL parameters
+    doctor = get_object_or_404(Doctor, id=doctor_id)
+    
+    # Retrieve all approved comments for the specified doctor
+    comments = Comment.objects.filter(doctor=doctor, is_approved=True).order_by('-created_at')
+    
+    return render(request, 'doctor_comments.html', {'doctor': doctor, 'comments': comments})
+
+def create_comment(request, doctor_id, visit_id):
+    # Retrieve doctor and visit based on URL parameters
+    doctor = get_object_or_404(Doctor, id=doctor_id)
+    visit = get_object_or_404(Visit, id=visit_id)
+    
+    if request.method == 'POST':
+        form = CommentForm(request.POST, doctor=doctor, visit=visit, user=request.user)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.user = request.user  # Set the logged-in user
+            comment.save()
+            return redirect('doctor_comments', doctor_id=doctor_id)
+    else:
+        form = CommentForm(doctor=doctor, visit=visit, user=request.user)
+
+    return render(request, 'create_comment.html', {'form': form, 'doctor': doctor})
