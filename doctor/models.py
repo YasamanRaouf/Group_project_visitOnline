3398b99148b2ad98022695714473f2b3f2caa7ba
Mahan MI <majlesi.mahan@gmail.com>
2024-09-17 01:29:04 +0330

    fix(doctor): :bug: default availability doctor model

--- doctor/models.py
+++ doctor/models.py
@@ -14,13 +14,14 @@ def default_availability():
         'interval': 20,
     }
 
+
 class Doctor(models.Model):
     doctor_id = models.AutoField(primary_key=True)
     user = models.ForeignKey('user.User', on_delete=models.CASCADE)
     specialty = models.ForeignKey('user.Specialty', on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     is_active = models.BooleanField(default=True)
-    availability = models.JSONField(default=dict(default_availability))
+    availability = models.JSONField(default=default_availability())
 
     def __str__(self):
         return f"Dr. {self.user.full_name} - {self.specialty.spec_name}"
@@ -32,7 +33,8 @@ class Doctor(models.Model):
 class Visit(models.Model):
     visit_id = models.AutoField(primary_key=True)
     doctor = models.ForeignKey('Doctor', on_delete=models.CASCADE)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
     date_time = models.DateTimeField()
 
     def __str__(self):
