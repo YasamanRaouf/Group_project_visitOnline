dae6dd83296e6fc7a8d465d80dc21523593f2431
i-ftm <ftm.nateghi@gmail.com>
2024-09-17 17:54:57 +0330

    admin

--- doctor/admin.py
+++ doctor/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from .models import Doctor
+from user.models import Profile
+
+@admin.register(Doctor,Profile)
+class DoctorAdmin(admin.ModelAdmin):
+    list_display = [
+        'doctor_id',
+        'user__full_name',
+        'specialty',
+        'price',
+        'is_active',
+    ]
+    
+    sortable_by = [
+        'doctor_id',
+    ]
+    
+    list_editable = [
+        'user__full_name',
+        'specialty',
+        'price',
+        'is_active',  
+    ]
+    
+    search_fields = [
+        'doctor_id',
+        'user__full_name',
+    ]
 
 # Register your models here.
