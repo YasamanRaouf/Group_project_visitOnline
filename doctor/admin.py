81e7b905076138069e5c045e95d9e1f84b5ac527
siamak kamanabroo <siamak.kamanabroo@gmail.com>
2024-09-18 22:40:20 +0330

    update doctor/admin, remove profile

--- doctor/admin.py
+++ doctor/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
 from .models import Doctor
-from user.models import Profile
 
-@admin.register(Doctor,Profile)
+@admin.register(Doctor)
 class DoctorAdmin(admin.ModelAdmin):
     list_display = [
         'doctor_id',
