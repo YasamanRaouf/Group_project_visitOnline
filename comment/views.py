37d992395ef984c60b2a864a011a734047defc3a
Mahan MI <majlesi.mahan@gmail.com>
2024-09-19 19:03:49 +0330

    Refactor comment templates and views
    - Refactor create_comment.html and doctor_comments.html templates to extend base.html for consistent styling.
    - Remove unnecessary HTML tags and update heading levels.
    - Add conditional rendering of messages in create_comment.html template.
    - Update imports in views.py to match model changes.
    - Reformat code for better readability.

--- comment/views.py
+++ comment/views.py
@@ -1,27 +1,28 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .forms import CommentForm
-from .models import Doctor, Visit
+from doctor.models import Doctor, Visit
+from .models import Comment
 
-# Create your views here.
-from django.shortcuts import render, get_object_or_404
-from .models import Comment, Doctor
 
 def doctor_comments(request, doctor_id):
     # Retrieve the doctor based on URL parameters
     doctor = get_object_or_404(Doctor, id=doctor_id)
-    
+
     # Retrieve all approved comments for the specified doctor
-    comments = Comment.objects.filter(doctor=doctor, is_approved=True).order_by('-created_at')
-    
+    comments = Comment.objects.filter(
+        doctor=doctor, is_approved=True).order_by('-created_at')
+
     return render(request, 'doctor_comments.html', {'doctor': doctor, 'comments': comments})
+
 
 def create_comment(request, doctor_id, visit_id):
     # Retrieve doctor and visit based on URL parameters
     doctor = get_object_or_404(Doctor, id=doctor_id)
     visit = get_object_or_404(Visit, id=visit_id)
-    
+
     if request.method == 'POST':
-        form = CommentForm(request.POST, doctor=doctor, visit=visit, user=request.user)
+        form = CommentForm(request.POST, doctor=doctor,
+                           visit=visit, user=request.user)
         if form.is_valid():
             comment = form.save(commit=False)
             comment.user = request.user  # Set the logged-in user
